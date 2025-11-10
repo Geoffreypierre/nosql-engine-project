83faca569bfb98a2f9afcb0b4e1c9c82b8b8278d
Hurel <jeremyhurel34@gmail.com>
2025-11-10 14:39:55 +0100

    To do implement proper iterator to  have lazy computing and up query speed

---
+++ src/main/java/qengine/util/SubstitutionIterator.java
@@ -0,0 +1,21 @@
+package qengine.util;
+
+import java.util.Iterator;
+
+import fr.boreal.model.logicalElements.api.Substitution;
+
+public class SubstitutionIterator implements Iterator<Substitution> {
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+    }
+
+    @Override
+    public Substitution next() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+
+}
